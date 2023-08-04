bf8ee10d39dfb3a43ce125375e7d48e694e95c70
glow <yuhui.liu@shopee.com>
2023-08-04 17:15:00 +0800

    feat: 实现布局组件

--- packages/components/main.ts
+++ packages/components/main.ts
@@ -2,7 +2,9 @@ import { createApp } from 'vue';
 //import Example from './example.vue';
 //import Example from './examples/theme.vue';
 // import Example from './examples/message.vue';
-import Example from './examples/dialog.vue';
+// import Example from './examples/dialog.vue';
+// import Example from './examples/grid.vue';
+import Example from './examples/layout.vue';
 import './src/index.less';
 
 const app = createApp(Example);
