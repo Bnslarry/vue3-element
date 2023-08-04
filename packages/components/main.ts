2a6ec129b4616f231c2a616fcc1b06fc910ab3df
glow <yuhui.liu@shopee.com>
2023-08-04 15:31:29 +0800

    feat: 添加动态渲染组件

--- packages/components/main.ts
+++ packages/components/main.ts
@@ -1,6 +1,8 @@
 import { createApp } from 'vue';
 //import Example from './example.vue';
-import Example from './examples/theme.vue';
+//import Example from './examples/theme.vue';
+// import Example from './examples/message.vue';
+import Example from './examples/dialog.vue';
 import './src/index.less';
 
 const app = createApp(Example);
